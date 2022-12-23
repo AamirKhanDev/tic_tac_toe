d09e35d7f58f58897584923b1152e81a7748939a
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-12-23 09:45:27 +0000

    add styling to board to make it look more like a playable tic tac toe game

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Background from '../images/Background.jpg'
+
+
+const style = {
+  backgroundImage: `url(${Background})`,
+  border: '2px solid darkblue',
+  fontSize: '30px',
+  fontWeight: '800',
+  cursor: 'pointer',
+  outline: 'none'
+};
 
 const Square = ({ value, onClick }) => (
-    <button onClick={onClick}>
-        {value}
-    </button>
+  <button style={style} onClick={onClick}>
+      {value}
+  </button>
 );
 
 export default Square;
+
